2fa70fc32cac33b4302ca063463fc3cc8a691680
Syrus Mesdaghi <syrusm@gmail.com>
2015-10-20 17:53:06 -0400

    adding map for shelter page

--- clean-build.sh
+++ clean-build.sh
@@ -10,5 +10,6 @@ ionic plugin add cordova-plugin-console
 ionic plugin add cordova-plugin-network-information
 ionic plugin add cordova-plugin-camera
 ionic plugin add https://github.com/wildabeast/BarcodeScanner.git
+ionic plugin add cordova-plugin-geofence
 ionic plugin add cordova-plugin-dialogs
 ionic plugin add https://github.com/EddyVerbruggen/cordova-plugin-actionsheet.git
